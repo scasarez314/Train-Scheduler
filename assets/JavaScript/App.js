9db0e3b06214ec3e629612d4b34aeac06db0d39a
Sierra Casarez <scasarez314@gmail.com>
2019-07-27 18:39:31 -0500

    uploading files

--- assets/JavaScript/App.js
+++ assets/JavaScript/App.js
@@ -1,6 +1,6 @@
 //Firebase key?
 var firebaseConfig = {
-    apiKey: ,
+    apiKey: "AIzaSyA65TxtdGwpxlN9Swc3ExOuwZeHOPIKgnw",
     authDomain: "trian-scheduler.firebaseapp.com",
     databaseURL: "https://trian-scheduler.firebaseio.com",
     projectId: "trian-scheduler",
@@ -22,6 +22,12 @@ var trainDestination = "";
 var trainFTT = "";
 var trainFrequency = "";
 
+database.ref().on("value", function (snapshot) {
+    // console.log(snapshot.val());
+
+})
+
+//onClick for the train Submit
 $("#add-Train").on("click", function (event) {
     //Prevent the page from refreshing
     event.preventDefault();
@@ -37,13 +43,13 @@ $("#add-Train").on("click", function (event) {
     $("#ftt-input").val("");
     $("#frequency-input").val("");
     //Console.log the value to check if they are being captured
-    console.log(trainName);
-    console.log(trainDestination);
-    console.log(trainFTT);
-    console.log(trainFrequency);
+    // console.log(trainName);
+    // console.log(trainDestination);
+    // console.log(trainFTT);
+    // console.log(trainFrequency);
 
     //Change what is saved in firebase
-    database.ref().set({
+    database.ref().push({
         train: trainName,
         destination: trainDestination,
         firsttraintime: trainFTT,
@@ -51,4 +57,61 @@ $("#add-Train").on("click", function (event) {
 
 
     });
+});
+
+//Firebase event
+database.ref().on("child_added", function (childSnapshot) {
+    // console.log(childSnapshot.val());
+
+    //Make everything a var
+    var trainName = childSnapshot.val().train;
+    var trainDestination = childSnapshot.val().destination;
+    var trainFTT = childSnapshot.val().firsttraintime;
+    var trainFrequency = parseInt(childSnapshot.val().frequency);
+    //Check to see if there is a call back
+    // console.log(trainName);
+    // console.log(trainDestination);
+    // console.log(trainFTT);
+    // console.log(trainFrequency);
+
+
+    //Moment.js stuff
+
+    //Var for the current time
+    var now = moment().format('LT');
+    console.log(now + " This is the current time.")
+
+    //Format time start time
+    var trainFTTValue = moment(trainFTT, "hh:mm").format("hh:mm a");
+    console.log(trainFTTValue + " This should be the formatted time")
+
+    //Next arrival 
+    var minsAway = moment().diff(moment(trainFrequency, "X"), "minutes");
+    console.log(minsAway);
+
+    var timeInbetween = minsAway % trainFrequency;
+
+    var minsDiff = trainFrequency - timeInbetween;
+    console.log(minsDiff);
+
+
+    //Format time back into HH:mm
+    var timeTheTrainWillArrive = moment().add(minsDiff, "minutes").format("hh:mm a");
+    console.log(timeTheTrainWillArrive);
+
+
+
+    //Make a new row and Append it to the tBody
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(trainDestination),
+        $("<td>").text(trainFTTValue),
+        $("<td>").text(trainFrequency),
+        $("<td>").text(timeTheTrainWillArrive),
+        $("<td>").text(minsDiff)
+
+
+    );
+    $("#train-table").append(newRow);
+
 });
